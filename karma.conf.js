f8809d4ea4a8e88231c90a29c3de818b9b390749
gsoos <soos.gabor86@gmail.com>
2017-06-10 16:24:54 +0200

    fix test runner

--- karma.conf.js
+++ karma.conf.js
@@ -2,10 +2,10 @@
 
 let ResolveFile = require('./lib/resolve-file');
 let Config = require('./lib/config');
-const config = new Config();
+const config = (new Config()).calculate();
 
 let karmaConfig = require('@ngx-webpack/karma')
-  .karma(ResolveFile.getWebpackConfig(), config.calculate());
+  .karma(ResolveFile.getWebpackConfig(), { useDlls: config.useDlls });
 
 module.exports = (config) => {
   config.set(karmaConfig);
