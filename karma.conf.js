ea2adb9a2c1f73d6baea03385a62cd40015c70e2
gsoos <soos.gabor86@gmail.com>
2017-06-10 15:31:20 +0200

    pass own config in entry scripts

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,11 @@
 'use strict';
 
 let ResolveFile = require('./lib/resolve-file');
-let karmaConfig = require('@ngx-webpack/karma').karma(ResolveFile.getWebpackConfig());
+let Config = require('./lib/config');
+const config = new Config();
+
+let karmaConfig = require('@ngx-webpack/karma')
+  .karma(ResolveFile.getWebpackConfig(), config.calculate());
 
 module.exports = (config) => {
   config.set(karmaConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 'use strict';
 
 let ResolveFile = require('./lib/resolve-file');
+let Config = require('./lib/config');
+const config = new Config();
 
-module.exports = require('@ngx-webpack/core').webpack({
+module.exports = require('@ngx-webpack/core').webpack(config.calculate({
   tsLint: ResolveFile.getTslintConfig()
-});
+}));
