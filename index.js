d057585ae75cdf5115624686d0b7308d525060fb
gsoos <soos.gabor86@gmail.com>
2017-08-17 14:58:05 +0200

    exported function include default options

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 'use strict';
 
-let ngxWebpack = require('@ngx-webpack/core');
-let ngxKarma = require('@ngx-webpack/karma');
-let Config = require('./lib/config');
+const ResolveFile = require('./lib/resolve-file');
+const ngxWebpack = require('@ngx-webpack/core');
+const ngxKarma = require('@ngx-webpack/karma');
+const Config = require('./lib/config');
 
 module.exports = {
-  webpack: ngxWebpack,
-  karma: ngxKarma,
+  webpack: function(options) {
+    const config = new Config();
+
+    return ngxWebpack.webpack(
+      config.calculate(
+        Object.assign({ tsLint: ResolveFile.getTslintConfig() }, options)
+      )
+    );
+  },
+  karma: function(options) {
+    const config = new Config();
+
+    return ngxKarma.karma(
+      ResolveFile.getWebpackConfig(),
+      { useDlls: config.calculate(options).useDlls }
+    )
+  },
   Config
 };
